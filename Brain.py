39d573dc9d7f8c38b60a3e6458e92003d39ada33
David <davidclark314@gmail.com>
2016-09-14 20:02:37 -0500

    Working learning engine that takes a command and learns what output is correct. Randomly guesses until it gets the right answer, then stores the answer with the command for future reference.

--- Brain.py
+++ Brain.py
@@ -5,7 +5,7 @@ from InputReceiver import InputReceiver
 from FileWriter import write
 
 class Brain:
-	OUTPUT_STATES = 10  # 10 digits
+	OUTPUT_STATES = 3  # 10 digits
 	ARRAY_SIZE = (OUTPUT_STATES)
 	THINKING_SPEED = 1  # Seconds per thinking loop
 	DECAY_RATE = 0.99  # Percent of memories kept at each iteration
@@ -16,75 +16,106 @@ class Brain:
 	VALID_OUTPUT = np.arange(10)
 
 	def __init__(self):
-		self.state = np.zeros(self.ARRAY_SIZE)
-		self.thinking = False
-		self.input_receiver = InputReceiver()
-		threading.Thread(target=self.input_receiver.receive).start()
+		self.state = None
+		self.command_state_memory = {}
+		self.last_alteration = None
 
-	# Runs every THINKING_SPEED seconds - skips a thinking loop if last loop is still running. This allows
-	#   us to limit thinking speed.
 	def live(self):
-		threading.Timer(self.THINKING_SPEED, self.live).start()
+		while True:
+			print()
+			self.state = self.get_neutral_state()
 
-		# If we are still thinking from last time, skip this run
-		if self.thinking: return
+			# Get a command
+			command = str(self.get_command())
 
-		# BEGIN THINKING
-		self.thinking = True
-		# write()
+			# Update brain state based on command
+			self.update_from_command(command)
+			# print(self.state)
 
-		# Get input
-		inputs = self.get_inputs()
+			# Get Feedback based on command
+			feedback = self.get_feedback()
 
-		# Update
-		self.update(inputs)
+			# Learn from feedback
+			self.learn(command, feedback)
 
-		# Output
-		self.output()
 
-		self.thinking = False
-		# END THINKING
+	def get_neutral_state(self):
+		return np.zeros(self.ARRAY_SIZE)
 
-	def get_inputs(self):
-		return self.input_receiver.return_inputs()
+	@staticmethod
+	def get_command():
+		command = input('Please give me a command: ')
+		return command
 
-	def update(self, inputs):
-		self.randomly_mutate()
-		self.decay()
-		self.learn_from_inputs(inputs)
+	def update_from_command(self, command):
+		try:
+			# We have seen this command before and got good feedback - let's try it again
+			self.state = self.command_state_memory[command]
+		except KeyError:
+			# We don't have a success state for this command - let's try something random and get some feedback
+			self.randomly_mutate()
 
 	def randomly_mutate(self):
-		self.state += np.random.uniform(-self.RANDOM_WEIGHT, self.RANDOM_WEIGHT, self.ARRAY_SIZE)
+		alteration = np.random.uniform(-self.RANDOM_WEIGHT, self.RANDOM_WEIGHT, self.ARRAY_SIZE)
+		self.state += alteration
+		self.last_alteration = alteration
 
-	def decay(self):
-		self.state *= self.DECAY_RATE
-
-	def learn_from_inputs(self, inputs):
-		for input in inputs:
-			alteration = self.map_input_to_alteration(input)
-			write(str(alteration))
-			self.state += alteration
-
-	def map_input_to_alteration(self, input):
-		alteration = np.zeros(self.ARRAY_SIZE)
-
-		try:
-			input = int(input)
-			if input in self.VALID_INPUT:
-				alteration[input] += self.REINFORCEMENT_RATE
-			else:
-				write('{0} is not a valid input'.format(str(input)))
-		except TypeError:
-			write('TypeError: {0} is not a valid input'.format(str(input)))
-
-		return alteration
+	def get_feedback(self):
+		feedback = input('Is this correct? -> {0}\n(y/n) --> '.format(self.output()))
+		while feedback not in ['n','y']:
+			feedback = input('Please provide feedback as either "y" or "n" --> ')
+		return feedback
 
 	def output(self):
-		# write(str(self.state))
-		write(str(np.argmax(self.state)))
+		return np.argmax(self.state)
+
+	def learn(self, command, feedback):
+		if feedback == 'n':
+			print('Ok, so that was not right. Let me try again....')
+			self.command_state_memory.pop(command, None)  # Remove any stored success state
+		else:
+			print('Great! I think I am understanding.')
+			self.save_command_state(command, self.state)
+
+	def save_command_state(self, command, success_state):
+		self.command_state_memory[command] = np.copy(success_state)
+		# print(self.command_state_memory)
+
+
+	# def get_inputs(self):
+	# 	return self.input_receiver.return_inputs()
+	#
+	# def update(self, inputs):
+	# 	self.randomly_mutate()
+	# 	self.decay()
+	# 	self.learn_from_inputs(inputs)
+	#
+
+	#
+	# def decay(self):
+	# 	self.state *= self.DECAY_RATE
+	#
+	# def learn_from_inputs(self, inputs):
+	# 	for input in inputs:
+	# 		alteration = self.map_input_to_alteration(input)
+	# 		write(str(alteration))
+	# 		self.state += alteration
+	#
+	# def map_input_to_alteration(self, input):
+	# 	alteration = np.zeros(self.ARRAY_SIZE)
+	#
+	# 	try:
+	# 		input = int(input)
+	# 		if input in self.VALID_INPUT:
+	# 			alteration[input] += self.REINFORCEMENT_RATE
+	# 		else:
+	# 			write('{0} is not a valid input'.format(str(input)))
+	# 	except TypeError:
+	# 		write('TypeError: {0} is not a valid input'.format(str(input)))
+	#
+	# 	return alteration
 
 
 if __name__ == '__main__':
-	os.remove('output.txt')
 	brain = Brain()
 	brain.live()
